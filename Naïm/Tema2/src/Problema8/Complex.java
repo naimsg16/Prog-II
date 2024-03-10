d81205b4dcff41cb2bf69a10bf0b6852ff5b310b
naimsg <naimsaadigallego@gmail.com>
2024-03-10 10:30:36 +0100

    Added operations

--- Naïm/Tema2/src/Problema8/Complex.java
+++ Naïm/Tema2/src/Problema8/Complex.java
@@ -12,6 +12,7 @@ public class Complex {
         setImaginaryPart(imaginaryPart);
     }
 
+
     public void setRealPart(double realPart){
         this.realPart = realPart;
     }
@@ -32,13 +33,88 @@ public class Complex {
         return Math.toDegrees(Math.atan(this.imaginaryPart / this.realPart));
     }
 
+    public double getRealPart(){
+        return this.realPart;
+    }
+
+    public double getImaginaryPart(){
+        return this.imaginaryPart;
+    }
+
+    public double getModule(){
+        return this.module();
+    }
+
+    public double getArgument(){
+        return this.argument();
+    }
+
+    //----------------------------------------------------
+    //Instance methods
+
+    public void add (Complex z){
+        this.realPart += z.realPart;
+        this.imaginaryPart += z.imaginaryPart;
+    }
+
+    public void substract(Complex z){
+        this.realPart -= z.realPart;
+        this.imaginaryPart -= z.imaginaryPart;
+    }
+
+    public void multiply (Complex z){
+        double module = this.module() * z.module();
+        double argument = this.argument() + z.argument();
+        this.realPart = module * Math.cos(argument);
+        this.imaginaryPart = module * Math.sin(argument);
+    }
+
+    public void divide (Complex z){
+        double module = this.module() / z.module();
+        double argument = this.argument() - z.argument();
+        this.realPart = module * Math.cos(argument);
+        this.imaginaryPart = module * Math.sin(argument);
+    }
+
+    //--------------------------------------------------------------------
+    //Static methods
+
+    public Complex add (Complex z1, Complex z2){
+        double real = z1.realPart + z2.realPart;
+        double imaginary = z1.imaginaryPart + z2.imaginaryPart;
+        return new Complex(real,imaginary);
+    }
+    public Complex substract (Complex z1, Complex z2){
+        double real = z1.realPart - z2.realPart;
+        double imaginary = z1.imaginaryPart - z2.imaginaryPart;
+        return new Complex(real,imaginary);
+    }
+    public Complex multiply (Complex z1, Complex z2){
+        double module = z1.module() * z2.module();
+        double argument = z1.argument() + z2.argument();
+        return new Complex(module * Math.cos(argument),module * Math.sin(argument));
+    }
+    public Complex divide (Complex z1, Complex z2){
+        double module = z1.module() / z2.module();
+        double argument = z1.argument() - z2.argument();
+        return new Complex(module * Math.cos(argument),module * Math.sin(argument));
+    }
+
+
+    public boolean equalsComplex(Complex z){
+        return this.realPart == z.realPart && this.imaginaryPart == z.imaginaryPart;
+    }
+
     public String toStringBinomial (){
         String formattedRealPart = new DecimalFormat("0.##").format(this.realPart);
         String formattedImaginaryPart = new DecimalFormat("0.##").format(this.imaginaryPart);
-        if(this.realPart == 0){
+        if (this.realPart == 0.0 && this.imaginaryPart == 0.0){
+            return "0";
+        }
+        if(this.realPart == 0.0){
             return formattedImaginaryPart + "i";
         }
-        if (this.imaginaryPart == 0){
+        if (this.imaginaryPart == 0.0){
             return formattedRealPart;
         }
         return formattedRealPart + " + " + formattedImaginaryPart + "i";
--- Naïm/Tema2/src/Problema8/Problema8.java
+++ Naïm/Tema2/src/Problema8/Problema8.java
@@ -4,9 +4,7 @@ import acm.program.*;
 public class Problema8 extends CommandLineProgram{
 
     public void run(){
-        Complex z = new Complex(1,1);
-        println(z.toStringBinomial());
-        println(z.toStringPolar());
+
     }
 
     public static void main(String[] args){
