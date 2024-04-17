63fbb945d0043c6ad6a85ec9d1ab3c2f53023042
naimsg <naimsaadigallego@gmail.com>
2024-04-17 20:30:31 +0200

    CharCounter

---
+++ Naïm/Tema3/src/CharCounter.java
@@ -0,0 +1,43 @@
+public class CharCounter {
+
+    private int counter;
+    private final String target;
+    private final boolean checkCase;
+
+    public CharCounter (String target, boolean checkCase){
+        this.counter = 0;
+        this.target = target;
+        this.checkCase = checkCase;
+    }
+    public void countIfTargeted(char c){
+        if(targeted(c)){
+            counter += 1;
+        }
+    }
+
+    private boolean targeted(char c){
+        if(!checkCase) {
+            char lowerC = Character.toLowerCase(c);
+            String lowerTarget = target.toLowerCase();
+            return found(lowerC, lowerTarget);
+        }
+        return found(c,target);
+    }
+
+    private boolean found(char c, String target){
+        for(int i = 0; i < target.length(); i++){
+            if(target.charAt(i) == c){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String toString(){
+        return String.valueOf(this.counter);
+    }
+
+
+
+
+}
---
+++ Naïm/Tema3/src/CountingVocals.java
@@ -0,0 +1,29 @@
+import acm.program.*;
+
+import java.io.*;
+
+public class CountingVocals extends ConsoleProgram {
+
+    private final static String FILE_NAME = "input.txt";
+    private final static String VOCALS = "aeiou";
+
+    public void run() {
+        try {
+            CharCounter counters = new CharCounter(VOCALS, false);
+            FileReader input = new FileReader(FILE_NAME);
+            int c = input.read();
+            while ( c != -1 ) {
+                 counters.countIfTargeted((char) c);
+                 c = input.read();
+            }
+            input.close();
+            println(counters.toString());
+        } catch (IOException ex) {
+             println("Something bad has happened :-(");
+        }
+    }
+
+    public static void main(String[] args) {
+        new CountingVocals().start(args);
+    }
+}
